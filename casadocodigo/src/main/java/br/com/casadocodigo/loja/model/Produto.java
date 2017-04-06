1599303c1bd7ceb65d838efda16f907c1cf5ca95
gustavo <gustavo.s.torres@hotmail.com>
2017-04-05 22:30:55 -0300

    Aula 3

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/model/Produto.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/model/Produto.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.model;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,43 +10,60 @@ import javax.persistence.Id;
 
 @Entity
 public class Produto {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String descricao;
 	private String paginas;
 	
+	@ElementCollection
+	private List<Preco> precos;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public String getPaginas() {
 		return paginas;
 	}
+
 	public void setPaginas(String paginas) {
 		this.paginas = paginas;
 	}
-	
+
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
+	}
+
 	@Override
 	public String toString() {
-		return titulo + "\n" + descricao +"\n"+paginas; 
+		return titulo + "\n" + descricao + "\n" + paginas;
 	}
-	
+
 }
--- casadocodigo/src/main/webapp/WEB-INF/views/produtos/form.jsp
+++ casadocodigo/src/main/webapp/WEB-INF/views/produtos/form.jsp
@@ -1,10 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>Cadastro de livros</title>
 </head>
 <body>
 	<form action="/casadocodigo/produtos" method="post">
@@ -20,6 +22,16 @@
 		<div>
 			<label>Páginas</label><input type="text" name="paginas">
 		</div>
+
+
+		<c:forEach items="${tipos}" var="tipoPreco" varStatus="status">
+			<div>
+				<label>${tipoPreco}</label> 
+				<input type="text"name=precos[${status.index}].valor> 
+				<input type="hidden"name=precos[${status.index}].tipo value="${tipoPreco}">
+			</div>
+		</c:forEach>
+
 
 		<button type="submit">Cadastrar</button>
 	</form>
